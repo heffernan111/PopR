97342b5ef77eb6089c324ac20b09dbb3372127cd
Unknown <david.heff.dh@googlemail.com>
2017-12-29 20:06:41 +0000

    changed theme, prepare classes
    waiting for RED and BLUE bubbles

---
+++ app/src/main/java/com/artesseum/popr/BlueBubbles.java
@@ -0,0 +1,8 @@
+package com.artesseum.popr;
+
+/**
+ * Created by apoca on 29/12/2017.
+ */
+
+public class BlueBubbles {
+}
--- app/src/main/java/com/artesseum/popr/MainActivity.java
+++ app/src/main/java/com/artesseum/popr/MainActivity.java
@@ -1,13 +1,63 @@
 package com.artesseum.popr;
 
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity{
+
+
+    Boolean gamePlay;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+/*        final ArrayList<Object> bubblesArray = new ArrayList<>();
+        bubblesArray.add(new RedBubbles());
+        bubblesArray.add(new BlueBubbles());*/
+
+
+
+
+        gamePlay = true;
+
+
+
+    Runnable myRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while(gamePlay = true){
+
+                addBubbles();
+
+
+            }
+
+        }
+
+        private void addBubbles() {
+
+
+
+
+        }
+    };
+
+    Thread myThread = new Thread(myRunnable);
+    myThread.start();
+
+
     }
+
+
+
 }
